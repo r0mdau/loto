2cf62b1e4474d05b1991509ceabe2d05ae203036
Romain DAUBY <romain.dauby@orange.com>
2014-01-31 15:38:28 +0100

    Initial commit, on peut lancer le loto

---
+++ js/lancerLoto.js
@@ -0,0 +1,40 @@
+function lancerLoto() {
+    var nombres = [];
+    for(var i = 0 ; i < 5; i++){
+        nombres[i] = leNumeroQuiSort(nombres);
+    }
+    nombres[5] = getRandomNombre(1, 11);
+    afficherResultats(nombres);                
+}
+
+function afficherResultats(nombres) {
+    for(var i = 0; i < nombres.length; i++){
+        $('#res'+i).html(nombres[i]);
+    }
+}
+
+function leNumeroQuiSort(nombres) {
+    var leNombre = getRandomNombre(1, 50);
+    if (nombrePasEncoreSorti(nombres, leNombre)) {
+        return leNombre;
+    }else return leNumeroQuiSort(nombres);
+}
+
+function nombrePasEncoreSorti(nombres, leNombre){
+    var booly = true;
+    for(var i = 0; i < nombres.length; i++){
+        if (nombres[i] == leNombre) {
+            booly = false;
+        }
+    }
+    return booly;
+}
+
+function getRandomNombre(min, max) {
+    // la probailite que Math.random() retourne 1 et quasi nulle, donc pour 49 nombres possibles,
+    // on met le maximum à 50
+    var nombre = parseInt(Math.random() * max);
+    if (nombre < min) {
+        return getRandomNombre(min, max);
+    }else return nombre;
+}
